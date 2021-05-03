2f506277f19131f07f5c5c08ac89009c73eac37b
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-05-03 09:07:21 -0300

    Project created

---
+++ backend/src/test/java/com/max/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
